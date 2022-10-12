4f7a27fd1a73a5a5040b1de627de5739bf2a1cd0
DESKTOP-4SJU0C0\LEEJH <zndn121@gmail.com>
2022-10-12 20:20:22 +0900

    messages.properties 적용 테스트 (REST 방식으로 데이터 전송)

---
+++ practice/src/main/java/com/practice/config/WebConfig.java
@@ -0,0 +1,51 @@
+package com.practice.config;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Encoding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration // 설정파일을 의미하는 어노테이션
+public class WebConfig {
+
+    @Bean // LocaleResolver Bean 생성 : 다국어처리 방법 선택
+    public LocaleResolver defaultLocaleResolver() {
+        //AceeptHeaderLocaleResolver :  웹 브라우저가 전송한 Accept-Language 헤더로부터 Locale 선택, setLocale() 지원X
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREAN); // locale의 기본값을 KOREA로 지정
+
+        log.info("localeResolver Bean Created.");
+        return localeResolver;
+    }
+
+    @Bean // ReloadableResourceBundleMessageSource Bean 생성 : 기본 MessageSource를 대체
+    public ReloadableResourceBundleMessageSource messageSource() {
+        Locale.setDefault(Locale.KOREAN); // 제공하지 않는 언어로 들어왔을 때 처리
+
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages/messages"); // 읽어올 messages.properties의 경로를 지정함
+        messageSource.setDefaultEncoding(Encoding.DEFAULT_CHARSET.toString()); // 인코딩 설정 : DEFAULT_CHARSET -> UTF-8
+        messageSource.setDefaultLocale(Locale.getDefault()); // Default Locale은 웹 브라우저가 전송한 값으로 설정
+        messageSource.setCacheSeconds(600); // 캐시 주기 설정, 기본값 : forever
+
+        log.info("messageSource Bean Created. Default Charset is {} and Default Locale is {}", Encoding.DEFAULT_CHARSET.toString(), Locale.getDefault());
+
+        return messageSource;
+    }
+
+    @Bean // MessageSourceAccessor bean 등록으로 MessageSource를 직접 사용하는 것보다 편리하게 작성
+    public MessageSourceAccessor messageSourceAccessor (@Autowired ReloadableResourceBundleMessageSource messageSource) {
+    	// 위에서 설정한 messageSource를 autowired로 가져와서 MessageSourceAccessor에 지정
+        return new MessageSourceAccessor(messageSource);
+    }
+
+}
--- practice/src/main/java/com/practice/controller/TestController.java
+++ practice/src/main/java/com/practice/controller/TestController.java
@@ -1,13 +1,26 @@
 package com.practice.controller;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.practice.service.TestService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class TestController {
+
+    private final MessageSourceAccessor messageSource;
+
 	@Autowired
 	private TestService service;
 
@@ -24,4 +37,50 @@ public class TestController {
 		System.out.println("2:::::::::::"+service.getUserInfo2(1));
 		return "test/test";
 	}
+
+	// Messages.properties에 있는 값을 사용하여 Rest방식으로 데이터 전달
+    @GetMapping("/testcall")
+    @ResponseBody
+    public ArrayList<String> test (Locale locale){
+
+    	// replace.test="ko 여기에 {0}값을 숫자 {1} 형태로 넣으면 치환변수로 쓸 수 있어요." 의 {0}, {1}에 할당될 값을 배열로 저장 
+        String[] replaceValues = new String[]{"바인딩변수1번값", "바인딩변수2번값"};
+
+        ArrayList<String> msgs = new ArrayList<>();
+        // messages.properties가 key=value 형식으로 작성되어 key를 호출하면 value가 ArrayList에 저장됨
+        // 기본값이 ko이므로(한국에서 접속하고 설정을 변경하지 않았기 때문) messages_ko.properties가 출력됨
+        msgs.add(messageSource.getMessage("test")); // ko test
+        msgs.add(messageSource.getMessage("like.this.comma.ok")); // ko 이렇게 콤마를 넣어서 구분값으로 써도 됩니다.
+        msgs.add(messageSource.getMessage("100")); // ko 숫자는 자동으로 String Key로 인식돼요
+        msgs.add(messageSource.getMessage("replace.test", replaceValues)); // ko 여기에 바인딩변수1번값값을 숫자 바인딩변수2번값 형태로 넣으면 치환변수로 쓸 수 있어요.
+
+        msgs.add(messageSource.getMessage("test", Locale.ENGLISH)); // Locale을 ENGLISH로 지정 -> messages_en.properties가 출력됨
+        msgs.add(messageSource.getMessage("like.this.comma.ok", Locale.ENGLISH));
+        msgs.add(messageSource.getMessage("100", Locale.ENGLISH));
+        msgs.add(messageSource.getMessage("replace.test", replaceValues,  Locale.ENGLISH));
+
+        msgs.add(messageSource.getMessage("test", locale)); // Locale의 기본값이 ko이므로 따로 설정하지 않으면 messages_ko.properties가 설정됨
+        msgs.add(messageSource.getMessage("like.this.comma.ok", locale));
+        msgs.add(messageSource.getMessage("100", locale));
+        msgs.add(messageSource.getMessage("replace.test", replaceValues, locale));
+
+        return msgs;
+        /* msgs 결과값
+         [
+		    "ko test",
+		    "ko 이렇게 콤마를 넣어서 구분값으로 써도 됩니다.",
+		    "ko 숫자는 자동으로 String Key로 인식되요",
+		    "ko 여기에 바인딩변수1번값값을 숫자 바인딩변수2번값 형태로 넣으면 치환변수로 쓸 수 있어요.",
+		    "en test",
+		    "en 이렇게 콤마를 넣어서 구분값으로 써도 됩니다.",
+		    "en 숫자는 자동으로 String Key로 인식되요",
+		    "en 여기에 바인딩변수1번값값을 숫자 바인딩변수2번값 형태로 넣으면 치환변수로 쓸 수 있어요.",
+		    "ko test",
+		    "ko 이렇게 콤마를 넣어서 구분값으로 써도 됩니다.",
+		    "ko 숫자는 자동으로 String Key로 인식되요",
+		    "ko 여기에 바인딩변수1번값값을 숫자 바인딩변수2번값 형태로 넣으면 치환변수로 쓸 수 있어요."
+		]
+         */
+    }
+
 }
