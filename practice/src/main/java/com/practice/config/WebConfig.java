3142a4ecc3305cab2b6a981bac47550d3d268bbd
DESKTOP-4SJU0C0\LEEJH <zndn121@gmail.com>
2022-10-16 18:26:06 +0900

    handlerinterceptoradapter deprecated 수정

--- practice/src/main/java/com/practice/config/WebConfig.java
+++ practice/src/main/java/com/practice/config/WebConfig.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
@@ -40,14 +40,12 @@ public class WebConfig implements WebMvcConfigurer {
     }
 }
 
-class PathLocaleInterceptor extends HandlerInterceptorAdapter {
+class PathLocaleInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
 
         String uri = req.getRequestURI();
 
-        // 서버에 올렸을때 영문 메시지를 쪽바로 못가져 오는 경우가 있는데 고럴때는
-        // Locale.ENGLISH 를 Locale.US 로 바꾸고 메시지 번들 파일명을 messages_en_US.properties 로 바꿔보셈
         // /en 로 시작하는 url 일 경우 로케일을 en으로 바꿈
         Locale locale = uri.startsWith("/en") ? Locale.ENGLISH : Locale.KOREA;
         LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(req);
